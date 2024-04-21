738cf6919614b25efa9108d5ec188fad56836ecb
doossee <84768497+doossee@users.noreply.github.com>
2024-04-21 17:11:55 +0500

    bug fix

--- api/management/serializers.py
+++ api/management/serializers.py
@@ -7,7 +7,7 @@ from src.management.models import *
 
 class ProfileMeta:
     exclude = ["password", "is_staff", "is_superuser", "verify_time", "verify_code"]
-    read_only_fields = ["username", "user_type", "created_at", "updated_at", "last_login", "date_joined",]
+    read_only_fields = ["username", "user_type", "created_at", "updated_at", "last_login", "date_joined", "avatar"]
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -15,7 +15,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta(ProfileMeta):
         model = User
-        read_only_fields = ProfileMeta.read_only_fields + ["avatar"]
+        read_only_fields = ProfileMeta.read_only_fields
 
 
 class ChangeAvatarSerializer(serializers.Serializer):
@@ -67,8 +67,16 @@ class MeDoctorSerializer(serializers.ModelSerializer):
 class PatientSerializer(serializers.ModelSerializer):
     """ """
 
-    class Meta(UserSerializer.Meta):
+    class Meta:
         model = Patient
+        fields = [
+            "id", "first_name", "last_name", "middle_name", "is_active", "phone", "birth_date",
+            "pinfl", "passport", "passport_attachment", "additional_information", "age", "gender",
+            "ethnicity", "social_group", "profession", "demographic_additional", "additional_phone_number", 
+            "email", "telegram_username", "region", "city", "mahalla", "street", "building",
+            "latitude", "longitude", "created_by", "curator", "district", "admission_data"
+        ]
+        read_only_fields = ProfileMeta.read_only_fields
 
 
 class MePatientSerializer(serializers.ModelSerializer):
